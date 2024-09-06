a7cbf3863e5ca9d5dd7639d6cb06515f53be460b
lucasMarinsSantos <lucasmarins2024@gmail.com>
2024-09-06 11:59:55 -0300

    Término lista 4

--- Python/Exercícios/Lista 4/ex5.py
+++ Python/Exercícios/Lista 4/ex5.py
@@ -1,10 +1,13 @@
-print ("Splita e separa!\n")
+print ("Splita e conta!\n")
+
 import re
 
 texto1 = re.sub(r"[^A-Za-z0-9\s]", "", '''The Python Software Foundation and the global Python community welcome and encourage participation by everyone. Our community is based on mutual respect, tolerance, and encouragement, and we are working to help each other live up to these principles. We want our community to be more diverse: whoever you are, and whatever your background, we welcome you''').lower().split()
-texto2 = [] 
+
 py = tuple("python")
+texto2 = []
+
 for palavra in texto1:
-    if palavra[0] in py or palavra[-1] in py:
+    if len(palavra) > 4 and any(letra in py for letra in palavra):
         texto2.append(palavra)
 print (texto2)
