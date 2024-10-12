20b0ebe88a1b8c9faaf6847432d326e6affe0ed1
lucasMarinsSantos <lucasmarins2024@gmail.com>
2024-10-12 00:23:38 -0300

    UPDATE PORTIFÓLIO

---
+++ Portifólio/app/static/javascript/script.js
@@ -0,0 +1,64 @@
+const playpause = document.querySelector(".playpause");
+const audio = document.getElementById("musicafundo");
+const volume = document.getElementById("volume");
+const icon = playpause.querySelector("i");
+
+playpause.addEventListener("click", function () {
+  if (audio.paused) {
+    audio.play();
+    icon.classList.remove("fa-play");
+    icon.classList.add("fa-pause");
+  } else {
+    audio.pause();
+    icon.classList.remove("fa-pause");
+    icon.classList.add("fa-play");
+  }
+});
+
+volume.addEventListener("input", function () {
+  audio.volume = volume.value;
+});
+
+const buttons = document.querySelectorAll('.button');
+let selectedButtonIndex = 0;
+let isNavigatingWithKeyboard = false;
+
+if (buttons.length > 0) {
+  buttons[0].classList.add('selected');
+}
+
+function removeSelectedClass() {
+  buttons.forEach(button => button.classList.remove('selected'));
+}
+
+function selectButton(index) {
+  removeSelectedClass();
+  buttons[index].classList.add('selected');
+  buttons[index].focus();
+}
+
+document.addEventListener('keydown', (event) => {
+  isNavigatingWithKeyboard = true;
+  if (event.key === 'ArrowDown' || event.key === 'ArrowRight') {
+    selectedButtonIndex = (selectedButtonIndex + 1) % buttons.length;
+    selectButton(selectedButtonIndex);
+  } else if (event.key === 'ArrowUp' || event.key === 'ArrowLeft') {
+    selectedButtonIndex = (selectedButtonIndex - 1 + buttons.length) % buttons.length;
+    selectButton(selectedButtonIndex);
+  }
+});
+
+buttons.forEach((button, index) => {
+  button.addEventListener('mouseover', () => {
+    if (!isNavigatingWithKeyboard) {
+      selectedButtonIndex = index;
+      selectButton(selectedButtonIndex);
+    } else {
+      removeSelectedClass(); 
+    }
+  });
+
+  button.addEventListener('mouseleave', () => {
+    isNavigatingWithKeyboard = false;
+  });
+});
