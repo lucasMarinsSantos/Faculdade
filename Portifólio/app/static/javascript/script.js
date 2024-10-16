8891328abc4eb762d086cf34e8db86ab03161a43
lucasMarinsSantos <lucasmarins2024@gmail.com>
2024-10-16 01:33:56 -0300

    UPDATE PORTIFOLIO

--- Portifólio/app/static/javascript/script.js
+++ Portifólio/app/static/javascript/script.js
@@ -2,6 +2,7 @@ const playpause = document.querySelector(".playpause");
 const audio = document.getElementById("musicafundo");
 const volume = document.getElementById("volume");
 const icon = playpause.querySelector("i");
+const audioNavegacao = document.getElementById("audioNavegacao");
 
 playpause.addEventListener("click", function () {
   if (audio.paused) {
@@ -19,46 +20,47 @@ volume.addEventListener("input", function () {
   audio.volume = volume.value;
 });
 
-const buttons = document.querySelectorAll('.button');
+const buttons = document.querySelectorAll(".button");
 let selectedButtonIndex = 0;
 let isNavigatingWithKeyboard = false;
 
 if (buttons.length > 0) {
-  buttons[0].classList.add('selected');
+  buttons[0].classList.add("selected");
 }
 
 function removeSelectedClass() {
-  buttons.forEach(button => button.classList.remove('selected'));
+  buttons.forEach((button) => button.classList.remove("selected"));
 }
 
 function selectButton(index) {
   removeSelectedClass();
-  buttons[index].classList.add('selected');
+  buttons[index].classList.add("selected");
   buttons[index].focus();
+  audioNavegacao.currentTime = 0;
+  audioNavegacao.play();
 }
 
-document.addEventListener('keydown', (event) => {
+document.addEventListener("keydown", (event) => {
   isNavigatingWithKeyboard = true;
-  if (event.key === 'ArrowDown' || event.key === 'ArrowRight') {
+  if (event.key === "ArrowDown" || event.key === "ArrowRight") {
     selectedButtonIndex = (selectedButtonIndex + 1) % buttons.length;
     selectButton(selectedButtonIndex);
-  } else if (event.key === 'ArrowUp' || event.key === 'ArrowLeft') {
-    selectedButtonIndex = (selectedButtonIndex - 1 + buttons.length) % buttons.length;
+  } else if (event.key === "ArrowUp" || event.key === "ArrowLeft") {
+    selectedButtonIndex =
+      (selectedButtonIndex - 1 + buttons.length) % buttons.length;
     selectButton(selectedButtonIndex);
   }
 });
 
 buttons.forEach((button, index) => {
-  button.addEventListener('mouseover', () => {
+  button.addEventListener("mouseover", () => {
     if (!isNavigatingWithKeyboard) {
       selectedButtonIndex = index;
       selectButton(selectedButtonIndex);
-    } else {
-      removeSelectedClass(); 
     }
   });
 
-  button.addEventListener('mouseleave', () => {
+  button.addEventListener("mouseleave", () => {
     isNavigatingWithKeyboard = false;
   });
 });
